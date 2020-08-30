6a5589390dfd5112eaa03d93b51aad03056c396b
sourav saha <souravsaha675@gmail.com>
2020-08-31 03:42:01 +0530

    first method

--- src/operators.java
+++ src/operators.java
@@ -1,19 +1,19 @@
 public class operators {
-    public static void main(String[] args){
-        double x=20.00d;
-        double y=80.00d;
-        double z=(double) (x+y)*100.00;
-        double a=(double)z%40.00d;
+    public static void main(String[] args) {
+        double x = 20.00d;
+        double y = 80.00d;
+        double z = (double) (x + y) * 100.00;
+        double a = (double) z % 40.00d;
 
         System.out.println(z);
         System.out.println(a);
 
-        boolean c= (a==0)?true:false;
+        boolean c = (a == 0) ? true : false;
 
 
         System.out.println(c);
 
-        if(!c)
-            System.out.println("Got some Reminder"+a);
+        if (!c)
+            System.out.println("Got some Reminder" + a);
     }
 }
