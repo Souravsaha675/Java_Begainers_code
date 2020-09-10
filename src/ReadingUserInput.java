b7f57ac686ae04b5e3727f933cd7f9c10a179055
sourav saha <souravsaha675@gmail.com>
2020-09-11 03:21:27 +0530

    Sum of the Input Numbers

---
+++ src/ReadingUserInput.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ReadingUserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+
+        while (count < 10) {
+            int order = count + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                count++;
+                sum += number;
+
+            } else {
+                System.out.println("Invalid number");
+            }
+            scanner.nextLine();
+        }
+
+        System.out.println("Sum = " + sum);
+        scanner.close();
+    }
+}
