f3927afd2a52469ec4097bb45d37df1eff08fb50
sourav saha <souravsaha675@gmail.com>
2020-09-15 00:05:19 +0530

    java this() & super() example

--- Java Class & Constructors/src/com/Inheritance/Rectangle.java
+++ Java Class & Constructors/src/com/Inheritance/Rectangle.java
@@ -1,26 +1,20 @@
 package com.Inheritance;
 
-public class Rectangle {
-    private int x;
-    private int y;
+public class Rectangle extends Shape {
+
     private int width;
-    private  int height;
+    private int height;
 
     //1st constructor
-    public Rectangle(){
-        this(0,0); // calls 2nd constructor
+    public Rectangle(int x, int y) {
+        this(x, y, 0, 0); // calls 2nd constructor
     }
+
 
     //2nd constructor
-    public Rectangle(int width,int height){
-        this(0,0,width,height); // calls 3rd constructor
-    }
-
-    //3rd constructor
-    public Rectangle(int x,int y,int width,int height){
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
+    public Rectangle(int x, int y, int width, int height) {
+        super(x, y); //calls constructor from parent(Shape)
+        this.width = width;
+        this.height = height;
     }
 }
